bcd47810c106d8392978e11c6b76eea248ffada1
Jake Naviasky <jake@commonwealth.im>
2020-08-26 17:43:53 -0400

    Add ropsten network option.

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,7 @@
-const PrivateKeyProvider = require ('./private-provider')
-var privateKey = "99B3C12287537E38C90A9219D4CB074A89A16E9CDB20BF85728EBD97C343E342";
+const EdgewarePrivateKeyProvider = require ('./private-provider')
+var edgewarePrivateKey = "99B3C12287537E38C90A9219D4CB074A89A16E9CDB20BF85728EBD97C343E342";
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = "cushion member minute around tired true over mad fun drip ginger auto horse abuse uphold";
 
 module.exports = {
   compilers: {
@@ -9,16 +11,12 @@ module.exports = {
   },
   networks: {
     development: {
-      provider: () => new PrivateKeyProvider(privateKey, "http://localhost:9933/", 42),
+      provider: () => new EdgewarePrivateKeyProvider(edgewarePrivateKey, "http://localhost:9933/", 42),
       network_id: 42
     },
-    live: {
-      provider: () => new PrivateKeyProvider(privateKey, "http://35.203.125.209:9933/", 42),
-      network_id: 42
-    },
-    ganache: {
-      provider: () => new PrivateKeyProvider(privateKey, "http://localhost:8545/", 42),
-      network_id: 42
+    ropsten: {
+      provider: () => new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/b19b8175e688448ead43a0ab5f03438a", 0),
+      network_id: 3
     }
   }
 }

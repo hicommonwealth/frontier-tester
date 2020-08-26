46e3bf0fb835899ed48eb1c80a12647d66fa2c8c
Jake Naviasky <jake@commonwealth.im>
2020-08-26 17:04:47 -0400

    Replace create test with contract value.

--- tests/testCreate.js
+++ tests/testCreate.js
@@ -4,10 +4,20 @@ const SubContract = artifacts.require("SubContract");
 contract("CreateContract test", async (accounts) => {
   it("should spawn subcontract", async () => {
     let c = await CreateContract.deployed();
-    await c.spawn();
-    const subAddress = await c.getSub();
-    const sub = await SubContract.at(subAddress);
-    const balance = await sub.getValue();
-    assert.equal(balance, "0");
+
+    // create without value
+    // await c.spawn({ from: accounts[0] });
+    // let subAddress = await c.getSub();
+    // let sub = await SubContract.at(subAddress);
+    // let balance = await sub.getValue();
+    // assert.equal(balance, "0");
+
+    // create with value
+    const value = '1000000000';
+    await c.spawnWithValue({ value, from: accounts[0] });
+    let subAddress = await c.getSub();
+    let sub = await SubContract.at(subAddress);
+    let balance = await sub.getValue();
+    assert.equal(balance, value);
   });
 });

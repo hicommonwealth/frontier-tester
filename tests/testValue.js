ca4e1ca034ce912f9aff77e1503959bd8bcc306b
Jake Naviasky <jake@commonwealth.im>
2020-08-26 12:48:27 -0400

    Fix value tests to add balance.

--- tests/testValue.js
+++ tests/testValue.js
@@ -1,14 +1,17 @@
 const ValueContract = artifacts.require("ValueContract");
-const BN = require('bn.js');
+const BN = web3.utils.BN;
 
 contract("ValueContract test", async (accounts) => {
   it("should have value", async () => {
     let c = await ValueContract.deployed();
     let valueStored = await c.getValue();
 
-    valueToSend = "10000000000";
-    const tx = await c.sendValue({ value: valueToSend, from: accounts[0] });
+    const balance = await web3.eth.getBalance(accounts[0]);
+    valueToSend = new BN("10000000000");
+    const tx = await c.sendValue({ value: valueToSend.toString(), from: accounts[0] });
     const updatedValue = await c.getValue();
-    assert.equal(updatedValue.toString(), valueStored.add(new BN(valueToSend)));
+    const updatedBalance = await web3.eth.getBalance(accounts[0]);
+    assert.equal(updatedValue.toString(), valueStored.add(valueToSend).toString(), "contract value wrong");
+    assert.equal(updatedBalance, (new BN(balance)).sub(valueToSend).toString(), "account balance wrong");
   });
 });

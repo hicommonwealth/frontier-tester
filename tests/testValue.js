5946dc15dc516516c906290f662414967fe38e41
Jake Naviasky <jake@commonwealth.im>
2020-08-26 12:35:00 -0400

    Update testValue test.

--- tests/testValue.js
+++ tests/testValue.js
@@ -1,14 +1,14 @@
 const ValueContract = artifacts.require("ValueContract");
+const BN = require('bn.js');
 
 contract("ValueContract test", async (accounts) => {
   it("should have value", async () => {
     let c = await ValueContract.deployed();
     let valueStored = await c.getValue();
-    assert.equal(valueStored.toString(), "0");
 
     valueToSend = "10000000000";
-    await c.sendValue({ value: valueToSend, from: accounts[0] });
-    valueStored = await c.getValue();
-    assert.equal(valueStored.toString(), valueToSend);
+    const tx = await c.sendValue({ value: valueToSend, from: accounts[0] });
+    const updatedValue = await c.getValue();
+    assert.equal(updatedValue.toString(), valueStored.add(new BN(valueToSend)));
   });
 });

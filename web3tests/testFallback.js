e8ba056fe704cf777db6e727385aee804cc97196
Jake Naviasky <jake@commonwealth.im>
2020-09-21 11:25:09 -0400

    Fill out fallback test.

--- web3tests/testFallback.js
+++ web3tests/testFallback.js
@@ -15,7 +15,24 @@ describe('Fallback test', async () => {
     FB.setProvider(web3.currentProvider);
     const c = await FB.new({ from: account });
 
-    // prepare a transaction call
-    // TODO??
+    // prepare an invalid function call
+    const balanceBefore = await web3.eth.getBalance(account);
+    const functionSig = web3.eth.abi.encodeFunctionSignature('myMethod()');
+    const valueSent = new web3.utils.BN(web3.utils.toWei('10', 'ether'));
+    const receipt = await web3.eth.sendTransaction({
+      from: account,
+      to: c.address,
+      value: valueSent.toString(),
+      gas: web3.utils.toWei('1', 'ether'),
+      data: functionSig,
+    });
+    const balanceAfter = await web3.eth.getBalance(account);
+    const balanceDiff = new web3.utils.BN(balanceBefore).sub(new web3.utils.BN(balanceAfter));
+
+    // ensure the value sent was (mostly) returned
+    assert.isTrue(balanceDiff.lt(valueSent));
+
+    // ensure some gas fees were spent
+    assert.isTrue(balanceDiff.gtn(0));
   });
 })

877eae2785b03e744a2b3a572f583b8a57a135bf
Jake Naviasky <jake@commonwealth.im>
2020-09-22 16:35:16 -0400

    Value test update for gas price (fails).

--- web3tests/testValue.js
+++ web3tests/testValue.js
@@ -16,16 +16,19 @@ describe("ValueContract test", async () => {
     let balance = await web3.eth.getBalance(account);
 
     let c = await Value.new({ from: account });
+    let createReceipt = await web3.eth.getTransactionReceipt(c.transactionHash);
+    let createGasUsed = web3.utils.toWei(`${createReceipt.gasUsed}`, 'gwei');
     let valueStored = await c.getValue.call({ from: account });
 
     assert.equal(valueStored, '0');
     const valueToSend = web3.utils.toWei('1', 'ether');
     const tx = await c.sendValue({ value: valueToSend, from: account, gasPrice: 1000000000 });
-    balance = await web3.eth.getBalance(account);
-
+    const txGasUsed = web3.utils.toWei(`${tx.receipt.gasUsed}`, 'gwei');
     const updatedValue = await c.getValue.call({ from: account });
     const updatedBalance = await web3.eth.getBalance(account);
+    const totalSpent = (new BN(createGasUsed)).add(new BN(txGasUsed)).add(new BN(valueToSend));
     assert.equal(updatedValue.toString(), (new BN(valueStored)).add(new BN(valueToSend)).toString(), "contract value wrong");
+    assert.equal(updatedBalance.toString(), (new BN(balance)).sub(totalSpent).toString(), "account balance wrong");
   });
 
   it("should setup ValueContract with non-zero value", async () => {
@@ -39,17 +42,21 @@ describe("ValueContract test", async () => {
     const BN = web3.utils.BN;
     let balance = await web3.eth.getBalance(account);
 
-    const sendValue = web3.utils.toWei('1', 'ether');
-    let c = await Value.new({ from: account, value: sendValue });
+    const initialSendValue = web3.utils.toWei('1', 'ether');
+    let c = await Value.new({ from: account, value: initialSendValue });
+    let createReceipt = await web3.eth.getTransactionReceipt(c.transactionHash);
+    let createGasUsed = web3.utils.toWei(`${createReceipt.gasUsed}`, 'gwei');
     let valueStored = await c.getValue.call({ from: account });
 
     assert.equal(valueStored, web3.utils.toWei('1', 'ether'));
     const valueToSend = web3.utils.toWei('1', 'ether');
     const tx = await c.sendValue({ value: valueToSend, from: account, gasPrice: 1000000000 });
-
-    const updatedValue = await c.getValue.call({ from: account });
+    const txGasUsed = web3.utils.toWei(`${tx.receipt.gasUsed}`, 'gwei');
     const updatedBalance = await web3.eth.getBalance(account);
+    const updatedValue = await c.getValue.call({ from: account });
+    const totalValueSent = (new BN(initialSendValue)).add(new BN(valueToSend));
+    const totalSpent = (new BN(createGasUsed)).add(new BN(txGasUsed)).add(totalValueSent);
     assert.equal(updatedValue.toString(), (new BN(valueStored)).add(new BN(valueToSend)).toString(), "contract value wrong");
-    assert.isTrue((new BN(updatedBalance)).lte((new BN(balance)).sub(new BN(valueToSend))), "account balance wrong");
+    assert.equal(updatedBalance.toString(), (new BN(balance)).sub(totalSpent).toString(), "account balance wrong");
   });
 });

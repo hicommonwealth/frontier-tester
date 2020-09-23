8e201822985eda7ca375bdc37c2102aaf2ca1468
Jake Naviasky <jake@commonwealth.im>
2020-09-23 14:40:38 -0400

    Remove direct precompile tests.

--- web3tests/testHashing.js
+++ web3tests/testHashing.js
@@ -22,79 +22,21 @@ describe('Hashing test', async () => {
     c = await Hashing.new({ from: account });
   });
 
-  it('should perform keccak256 in contract', async () => {
+  it('should perform keccak256', async () => {
     const contractResult = await c.callKeccak256.call(messageHex, { from: account });
     const localResult = web3.utils.keccak256(messageHex);
     assert.equal(contractResult, localResult);
   });
 
-  // NOTE: Keccak256 does not have a precompile, so we cannot access it directly
-
-  it('should perform ripemd160 in contract', async () => {
+  it('should perform ripemd160', async () => {
     const contractResult = await c.callRipemd160.call(messageHex, { from: account });
     const localResult = new RIPEMD160().update(messageHex).digest('hex');
     assert.equal(contractResult, localResult);
   });
 
-  it('should perform ripemd160 directly', async () => {
-    const RIPEMD160_PRECOMPILE_ADDRESS = '0000000000000000000000000000000000000003';
-  
-    const RAW_TX = {
-      from: account,
-      gasPrice: "0x01",
-      gas: "0x10000000",
-      to: RIPEMD160_PRECOMPILE_ADDRESS,
-      value: "0x0",
-      data: messageHex,
-    };
-
-    const SIGNED_TX = await web3.eth.accounts.signTransaction(
-      RAW_TX,
-      privKey
-    );
-
-    const tx = await web3.eth.sendTransaction({
-      from: account,
-      to: RIPEMD160_PRECOMPILE_ADDRESS,
-      value: '0x0',
-      gas: '0x10000000',
-      data: messageHex,
-    });
-
-    assert.equal(tx.transactionHash, SIGNED_TX.transactionHash);
-  });
-
-  it('should perform sha256 in contract', async () => {
+  it('should perform sha256', async () => {
     const contractResult = await c.callSha256.call(messageHex, { from: account });
     const localResult = sha256.hex(messageHex);
     assert.equal(contractResult, localResult);
-  });
-
-  it('should perform sha256 directly', async () => {
-    const SHA256_PRECOMPILE_ADDRESS = '0000000000000000000000000000000000000002';
-   
-    const RAW_TX = {
-      from: account,
-      gasPrice: "0x01",
-      gas: "0x10000000",
-      to: SHA256_PRECOMPILE_ADDRESS,
-      value: "0x0",
-      data: messageHex,
-    };
-
-    const SIGNED_TX = await web3.eth.accounts.signTransaction(
-      RAW_TX,
-      privKey
-    );
-
-    const tx = await web3.eth.sendTransaction({
-      from: account,
-      to: SHA256_PRECOMPILE_ADDRESS,
-      value: '0x0',
-      gas: '0x10000000',
-      data: messageHex,
-    });
-
-    assert.equal(tx.transactionHash, SIGNED_TX.transactionHash);
   });
 });

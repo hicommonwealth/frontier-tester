867c22b876ca393327b9f74d1594e2c59360c573
dillchen <dillon@commonwealth.im>
2020-11-26 11:29:40 +0000

    fix testOwner test

--- web3tests/testOwner.js
+++ web3tests/testOwner.js
@@ -1,13 +1,26 @@
 const { assert } = require('chai');
-const { deployContract, account } = require('../utils');
 const OwnerContract = require('../build/contracts/OwnerContract.json');
+const contract = require("@truffle/contract");
+const { deployContract, account, initWeb3 } = require('../utils');
 
 describe("OwnerContract test", async () => {
+  let OC;
+  let web3;
+
+  before(async function() {
+    web3 = initWeb3();
+
+    OCDeploy = contract({
+      abi: OwnerContract.abi,
+      unlinked_binary: OwnerContract.bytecode,
+    });
+    OCDeploy.setProvider(web3.currentProvider);
+    OC = await OCDeploy.new({ from: account });
+  });
+
   it("should have owner", async () => {
-    let c = await deployContract('OwnerContract', OwnerContract);
-    let result = await c.methods.makeCall().call({
+    let result = await OC.makeCall({
       from: account,
-      gasPrice: 1000000000,
     });
     assert.isTrue(result);
   });
@@ -16,9 +29,8 @@ describe("OwnerContract test", async () => {
     // NOTE: this will fail, because the error will be:
     //     { code: -32603, message: 'inner executing call failed' }
     // rather than a revert!
-    let c = await deployContract('OwnerContract', OwnerContract);
     try {
-      await c.methods.makeCall.call({ from: '0xF8cef78E923919054037a1D03662bBD884fF4edf' });
+      await OC.makeCall({ from: '0xF8cef78E923919054037a1D03662bBD884fF4edf' });
       assert.fail('should throw');
     } catch (e) {
       
